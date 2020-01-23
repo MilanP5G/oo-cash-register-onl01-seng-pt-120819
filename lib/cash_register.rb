fe8bea5468d00f7b35ee14e3a6efeb56c6d89f49
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-01-23 18:20:16 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,22 @@
+require 'pry'
+
 class CashRegister
   attr_accessor :total, :items, :last_transaction, :discount
+  
+  def total=(total)
+    @total = total
+  end 
+  
+  def total
+    @total
+  end 
   
   
   def initialize(discount = 0)
     @discount = discount
     @total = 0 
     @items = []
-    @last_transaction = []
+    # @last_transaction = []
   end 
   
   def add_item(title,price,quantity = 1)
@@ -19,15 +29,17 @@ class CashRegister
     else
       @items << title
     end
-    @total += price * quantity
-    @last_transaction << (price * quantity) 
-    @total
+    self.total += price * quantity
+    @last_transaction = (price * quantity) 
+    self.total
   end
   
   def apply_discount
     if @discount > 0 
-      @discount = @discount/100.to_f 
-      @total = @total - (@total * (@discount)) 
+      # binding.pry
+      # @discount = @discount/100.to_f 
+      discounted = (@discount/100.to_f) * @total
+      @total = @total - discounted
       "After the discount, the total comes to $#{@total.to_i}."
     else 
       "There is no discount to apply."
@@ -36,7 +48,7 @@ class CashRegister
 
   
   def void_last_transaction
-    @total -= (@last_transaction[-1])
+    @total -= @last_transaction
   end 
   
 end 
